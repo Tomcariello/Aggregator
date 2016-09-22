f79dfb3f7ff86f7ee87b98f0218fefadb7b198ac
aanchal <aanchal.g.gupta@gmail.com>
2016-09-21 20:25:59 -0400

    seatGeek.js modified

--- assets/js/seatGeek.js
+++ assets/js/seatGeek.js
@@ -4,4 +4,22 @@
 // Aggregator
 // Client ID:  NTc3MjcxNHwxNDc0NDk4NTQx
 // Secret: u8sVtIcKXsxPJxTrt8WTfkfRLYCvHXjlDMt2TD3W
-console.log();
+ 
+function displaySportInfo(sport,postal_code){
+   // var sport = soccer;
+  // var postal_code = 08540;
+  
+  var queryURL = 'https://api.seatgeek.com/2/search?q=' + sport + '&'+postal_code + '&client_id=NTc3MjcxNHwxNDc0NDk4NTQx';
+$.ajax({
+  url:querURL,
+  method:'GET'
+})
+$('#seatgeek').html("Search Seatgeek for " + searchTermToSearch + zipCodeToSearch);
+
+}
+
+
+
+
+
+
