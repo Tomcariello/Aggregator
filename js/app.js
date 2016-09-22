7a186f0e2a5bfeae63e9539350c45c7aeed6bd41
Tomcariello <tomcariello@yahoo.com>
2016-09-21 21:06:40 -0400

    fixed js errors, sending zip code to seatgeek

--- js/app.js
+++ js/app.js
@@ -3,17 +3,21 @@ var searchTermToSearch;
 
 $('#submit').on('click', function() {
   console.log("Submit button clicked.");
-  zipCodeToSearch = $('#zipCode').val().trim;
-  var zipCodeStatus = checkZipCode(zipCodeToSearch);
-  console.log("zip code status is " + zipCodeStatus);
+  zipCodeToSearch = $('#zipCode').val().trim();
+  console.log("zip code to search is " + zipCodeToSearch);
   searchTermToSearch = $('#searchTerm').val().trim();
   displaySportInfo(searchTermToSearch,zipCodeToSearch);
   $('#bettingOdds').html("Search JSONOdds for " + searchTermToSearch);
   
 })
 
-function checkZipCode(zipStringToTest){
-  var parsedZip = parseInt(zipStringToTest);
-  console.log(/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(parsedZip));
-}
+// function checkZipCode(zipCodeToSearch){
+//   if (zipCodeToSearch.length == "5") {
+//     if (parseInt(zipCodeToSearch) == true {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
 
