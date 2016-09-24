54784ea0f62093ad6f3ae3e7015d26f20bc00443
Melissa414 <Missy41491@gmail.com>
2016-09-24 11:55:46 -0400

    fixed api in jsonodds

--- js/jsonOdds.js
+++ js/jsonOdds.js
@@ -1,9 +1,22 @@
-function getSportsObject(sport){
-  var url = 'https://crossorigin.me/https://jsonoddsapi.herokuapp.com/sports/'+sport
+function getOdds(){
+  var url = 'https://crossorigin.me/https://jsonoddsapi.herokuapp.com/odds';
 
   $.ajax({url: url, method: 'GET'}).done(function(response){
-    return response;
+    // change this to handle the response
+    console.log(response);
+  });
+}
+
+function getSportsObject(sport){
+  var url = 'https://crossorigin.me/https://jsonoddsapi.herokuapp.com/sports/'+sport;
+
+  $.ajax({url: url, method: 'GET'}).done(function(response){
+    // change this to handle the response
+    console.log(response);
   });
 }
 
 getSportsObject('MLB');
+
+getOdds();
+
