098e987ae9d7f309daf33298500408ee47a8de9e
Melissa414 <Missy41491@gmail.com>
2016-09-24 11:39:47 -0400

    testing mirror api

--- js/jsonOdds.js
+++ js/jsonOdds.js
@@ -1,37 +1,5 @@
-// var  sports = [];
+var url = 'https://crossorigin.me/https://jsonoddsapi.herokuapp.com/sports/MLB'
 
-// var apiKey = "bb90e449-8058-11e6-8199-067e79ca11d3";
-// var queryUrl = "https://jsonodds.com/api/odds/";
-
-// $.ajax({
-//         url: queryUrl,
-//         method: "Get",
-//         beforeSend: function(xhr){xhr.setRequestHeader("jsonodds-api-key", apiKey)},
-//       })
-//       .done(function(response) {
-//           console.log("does it work", response);
-
-//       });
-// function showSport(){
-
-//       $("#bettingOdds").empty();
-//   }
-
-//   console.log("something")
-//   
-//   
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://crossorigin.me/https://jsonodds.com/api/odds/",
-  "method": "GET",
-  "headers": {
-    "jsonodds-api-key": "bb90e449-8058-11e6-8199-067e79ca11d3",
-    "cache-control": "no-cache",
-    "postman-token": "f111c2e1-cfdd-7884-b714-a9d3d48dd37c"
-  }
-}
-
-$.ajax(settings).done(function (response) {
+$.ajax({url: url, method: 'GET'}).done(function(response){
   console.log(response);
 });
