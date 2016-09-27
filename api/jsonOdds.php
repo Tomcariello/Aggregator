8fdb19942bdfb78127b2e17d4b22e6cc774ed9c7
Melissa414 <Missy41491@gmail.com>
2016-09-26 20:26:56 -0400

    more changes

--- api/jsonOdds.php
+++ api/jsonOdds.php
@@ -56,7 +56,7 @@ $content .= '</tr>';
 
     $content.= $game['HomeTeam'];
     $content.= '</td><td>';
-    $content.= $game['AwayTeam'];
+    $content.= $game['PointSpreadHome'];
     $content.= '</td><td>';
     // $content.= $game['MatchTime'];
     $content.= '</td>';
@@ -68,7 +68,7 @@ $content .= '</tr>';
           // $content.= '</td><td>';
           // $content.= $odds['MoneyLineAway'];
           // $content.= '</td><td>';
-          $content.= $odds['PointSpreadHome'];
+          $content.= $odds['AwayTeam'];
           $content.= '</td><td>';
           // $content.= $odds['PointSpreadAway'];
           // $content.= '</td><td>';
