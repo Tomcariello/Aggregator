dda8a30dc0d2a3e271df4e4984114995cc65e349
aanchal <aanchal.g.gupta@gmail.com>
2016-09-29 10:42:14 -0400

    modified date format in seatGeek.js

--- js/seatGeek.js
+++ js/seatGeek.js
@@ -31,7 +31,7 @@ function printSportInfo(currentSeatResult){
         // Storing the value of a dynamically created row in a variable
         var newHeader = $('<tr>');
         // Adding columns under row and pushing data respectively into it.
-        newHeader.append($("<th>").html("Event:"));
+        newHeader.append($("<th>").html("Event"));
         newHeader.append($("<th>").html("Price"));
         newHeader.append($("<th>").html("Date"));
         newHeader.append($("<th>").html("Time"));
@@ -52,25 +52,28 @@ function printSportInfo(currentSeatResult){
           var titleLink = $('<a>').attr('href', currentSeatResult.events[i].url).attr("target", "new").html(currentSeatResult.events[i].short_title);
           var lowestPriceLink = $('<a>').attr('href', currentSeatResult.events[i].url).attr("target", "new").html( lowestPrice  +  highestPrice );
           // to seperate  date from time, we use split().
-
           var convertedDateTime = currentSeatResult.events[i].datetime_local.split("T").join(" & ");
           var timeSplit = currentSeatResult.events[i].datetime_local.split("T");
           var date = timeSplit[0];
+          // created a variable which stores date in a new Date function
+           var d =new Date(date) ;
           var time = timeSplit[1];
           // to remove seconds display from the time.
           var secRemoveTime = time.split("",5);
-
           var hours =time.split(":");
-          console.log(hours[0]);
+          // console.log(hours[0]);
           var ampm = "AM"
           // conversion of hours from military time to standard time
           if (hours[0] > 12) {
             hours[0] = hours[0]-12;
             ampm = "PM";
-            console.log(hours[0]);
+            // console.log(hours[0]);
           }
-          console.log(ampm)
-          var dateLink = $('<a>').attr('href', currentSeatResult.events[i].url).attr("target", "new").html(date);
+          // console.log(ampm)
+          // to get attribute of date and storing its value in a variable and making it into a link with href attribute.
+          // toDateString converts the date into more readable format.
+          var dateLink = $('<a>').attr('href', currentSeatResult.events[i].url).attr("target", "new").html(d.toDateString());
+          // to get attribute of time and storing its value in a variable and making it into a link with href attribute.
           var timeLink = $('<a>').attr('href', currentSeatResult.events[i].url).attr("target", "new").html(hours[0] + ":" + hours[1] +  ampm);
           // Appending these links to their respective columns.
           newRow.append($("<td>").html(titleLink));
