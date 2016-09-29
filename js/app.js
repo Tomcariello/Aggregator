7b26acd62d8e421529e40207016198a57f330855
Jesse Happel <happel.jesse@gmail.com>
2016-09-28 20:31:46 -0400

    improved image

--- js/app.js
+++ js/app.js
@@ -91,7 +91,7 @@ function changeBackground(sport) {
 
 timerTarget = setInterval(function(){
   changeJumbotron();
-},5000); 
+},1000); 
 
 function changeJumbotron(){
   $('.intro').css("background-image", "url(" + displayImages[i] + ")");
